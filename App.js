95bbe445dc10ac83ba0931f68997b5810a2f87a4
codershashtag1 <darshanapandey123@gmail.com>
2023-09-21 22:15:14 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,10 @@
+let element = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])
+);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
