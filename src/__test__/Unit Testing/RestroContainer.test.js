95d2a541eecd921e9065b6c4d596e366ef9d60ba
codershashtag1 <darshanapandey123@gmail.com>
2025-05-24 19:26:42 +0530

    Learn how to write Test Case for HOC

---
+++ src/__test__/Unit Testing/RestroContainer.test.js
@@ -0,0 +1,100 @@
+import { render, screen } from "@testing-library/react"
+import RestroContainer, { showDiscount } from "../../components/RestroContainer"
+import RestroContainerMockData from "../MockData/RestroContainerMockData.json"
+import "@testing-library/jest-dom"
+
+const renderRestaurantContainer = (clonedMockData) => {
+  return render(<RestroContainer resData={clonedMockData}/>)
+}
+
+const renderShowDiscount = (clonedMockData) => {
+  const DiscountComponent = showDiscount(RestroContainer)
+  return render(
+    <DiscountComponent 
+      resData={clonedMockData} 
+      discountOffer = {clonedMockData.card?.info?.aggregatedDiscountInfoV3}
+    />)
+}
+
+
+describe("Should render the card and card Details", () => {
+  let clonedMockData;
+  beforeEach(() => {
+    clonedMockData = RestroContainerMockData
+    renderRestaurantContainer(clonedMockData)
+  })
+
+  it("Should render image of the restuarant", async () => {
+    const image = await screen.getByRole("img");
+    expect(image).toBeInTheDocument()
+  })
+
+  it("Should render name of the restaurant", () => {
+    const name = screen.getByTestId("restro-name")
+    expect(name).toHaveTextContent("New Banana Leaf")
+  })
+
+  it("Should render avgRating of the restaurant", () => {
+    const avgRating = screen.getByTestId("restro-avgRating")
+    expect(avgRating).toHaveTextContent("4.4")
+  })  
+
+  it("Should render cuisines of the restaurant", () => {
+    const cuisines = screen.getByTestId("restro-cuisines");
+    expect(cuisines).toHaveTextContent("South Indian, Chinese, Fast Food")
+  })
+
+  it("Should render location of the restaurant", () => {
+    const location = screen.getByTestId("restro-location");
+    expect(location).toHaveTextContent("Dombivli East, Dombivli East")
+  })
+})
+
+describe("Should Render High Order Component", () => {
+  let clonedMockData;
+
+  beforeEach(() => {
+    clonedMockData = JSON.parse(JSON.stringify(RestroContainerMockData))
+  })
+
+  it("should render dicountOffer with header and subHeader", () => {
+    renderShowDiscount(clonedMockData)
+    let discountOffer = screen.getByTestId("discount-offer")
+    expect(discountOffer).toHaveTextContent("ITEMS AT ₹169")
+  })
+
+  it("Should render dicountOffer with header and without subHeader", () => {
+    delete clonedMockData.card.info.aggregatedDiscountInfoV3.subHeader
+    renderShowDiscount(clonedMockData)
+    let discountOffer = screen.getByTestId("discount-offer")
+    expect(discountOffer).toHaveTextContent("ITEMS")
+  })
+
+  it("Should render dicountOffer without header and with subHeader", () => {
+    delete clonedMockData.card.info.aggregatedDiscountInfoV3.header
+    console.log(clonedMockData.card.info.aggregatedDiscountInfoV3);
+    renderShowDiscount(clonedMockData)
+      let discountOffer = screen.getByTestId("discount-offer")
+      expect(discountOffer).toHaveTextContent("AT ₹169")
+  })
+
+  it("Should render dicountOffer without header and subHeader", () => {
+    delete clonedMockData.card.info.aggregatedDiscountInfoV3.header
+    delete clonedMockData.card.info.aggregatedDiscountInfoV3.subHeader
+    renderShowDiscount(clonedMockData);
+    let discountOffer = screen.queryByTestId("discount-offer")
+    expect(discountOffer).not.toBeInTheDocument()
+  })
+})
+
+describe("No props available", () => {
+  let clonedMockData = {};
+
+  it("should not render name of the restaurant", () => {
+    console.log(clonedMockData);
+    renderRestaurantContainer(clonedMockData);
+    let name = screen.queryByText(/ /);
+    expect(name).not.toBeInTheDocument()
+  })
+})
+
--- src/components/RestroContainer.js
+++ src/components/RestroContainer.js
@@ -1,6 +1,7 @@
 import { CDN_URL} from '../utils/constants';
 
 const RestroContainer = (props) => {
+
   let { resData } = props;
   const {
     name,
@@ -18,13 +19,15 @@ const RestroContainer = (props) => {
         /* transition delay-75 duration-100 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gray-500 */ }
         <img className = "bg-gray-500 shadow-lg shadow-gray-500/50 h-[250px] w-[400px] rounded-3xl transform transition hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
         src = { CDN_URL + cloudinaryImageId}
-        alt = "Example" />
+        alt = "Example" 
+        data-testid="food-img"
+        />
       </div>
       <div className="restro-img-description">
-        <h3 className="text-2xl font-bold">{name}</h3>
-        <h4 className="text-1xl font-semibold">{avgRating}</h4>
-        <h4 className="text-1xl font-semibold">{cuisines?.join(', ')}</h4>
-        <h4 className="text-1xl font-semibold">{areaName}, {locality}</h4>
+        <h3 data-testid="restro-name" className="text-2xl font-bold">{name}</h3>
+        <h4 data-testid="restro-avgRating" className="text-1xl font-semibold">{avgRating}</h4>
+        <h4 data-testid="restro-cuisines" className="text-1xl font-semibold">{cuisines?.join(', ')}</h4>
+        <h4 data-testid="restro-location" className="text-1xl font-semibold">{areaName}, {locality}</h4>
       </div>
     </div>
   );
@@ -37,10 +40,13 @@ export const showDiscount = (RestroContainer) => {
 
     return (
       <div className="relative w-fit">
-        {/* Discount badge positioned over the image */}
-        <div className="absolute top-2 text-white px-2 py-1 rounded z-10 text-4xl font-bold shadow">
-          {header} {(subHeader != undefined) ? subHeader : ""}
-        </div>
+        { 
+          header != undefined || subHeader != undefined ? (
+            <div data-testid = "discount-offer" className = "absolute top-2 text-white px-2 py-1 rounded z-10 text-4xl font-bold shadow" >
+              {header} {(subHeader != undefined) ? subHeader : ""}
+            </div>
+          ) : ""
+        }
         <RestroContainer resData={resData} />
       </div>
     );
